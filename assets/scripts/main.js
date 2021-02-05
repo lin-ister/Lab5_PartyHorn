e9dda09017e6cfc6038a3c5e58be75473d80f577
lin-ister <bplin@ucsd.edu>
2021-02-05 00:22:10 -0800

    "radio"

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,28 +2,29 @@
 
 <fieldset id="audio-selection">
     <div id="radio-air-horn-container">
-    <input type="radio" id="radio-air-horn" name="radio-sound" checked>
+    <input type="radio" id="radio-air-horn" name="radio-sound" onClick="change()" checked>
     <label for="radio-air-horn">Air Horn</label>
     </div>
     <div id="radio-car-horn-container">
-    <input type="radio" id="radio-car-horn" name="radio-sound">
+    <input type="radio" id="radio-car-horn" name="radio-sound" onClick="change()">
     <label for="radio-car-horn">Car Horn</label>
     </div>
     <div id="radio-party-horn-container">
-    <input type="radio" id="radio-party-horn" name="radio-sound">
+    <input type="radio" id="radio-party-horn" name="radio-sound" onClick="change()">
     <label for="radio-party-horn">Party Horn</label>
     </div>
 </fieldset>
 
-
-if(document.getElementById("radio-air-horn").checked){
-    document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
-}
-else if(document.getElementById("radio-car-horn").checked){
-    document.getElementById("sound-image").src = "./assets/media/images/car.svg";
-}
-else if(document.getElementById("radio-party-horn").checked){
-    document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+function change(){}
+    if(document.getElementById("radio-air-horn").checked){
+        document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    }
+    else if(document.getElementById("radio-car-horn").checked){
+        document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    }
+    else if(document.getElementById("radio-party-horn").checked){
+        document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+    }
 }
 
 function horn(){
