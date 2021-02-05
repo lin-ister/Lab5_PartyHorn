5e1ac12c8801b68dd136102ec172e204bb1b3964
lin-ister <bplin@ucsd.edu>
2021-02-04 23:58:47 -0800

    "Radio"

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,16 @@
 // main.js
 
+if(document.getElementById("radio-air-horn").checked){
+    document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+}
+else if(document.getElementById("radio-car-horn").checked){
+    document.getElementById("sound-image").src = "./assets/media/images/car-horn.svg";
+}
+else if(document.getElementById("radio-party-horn").checked){
+    document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+}
 
 function horn(){
-    if(document.getElementById("radio-air-horn").checked){
-        document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
-    }
-    else if(document.getElementById("radio-car-horn").checked){
-        document.getElementById("sound-image").src = "./assets/media/images/car-horn.svg";
-    }
-    else if(document.getElementById("radio-party-horn").checked){
-        document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
-    }
-}
 
     horn1=document.getElementById('radio-air-horn');
     horn2=document.getElementById('radio-car-horn');
